ca728057cfb0016f2087ca225584242ad1c5e115
Ross Davies <davies.ross@gmail.com>
2022-01-19 10:28:48 -0700

    Update security_firewalld_disable.sh
    Added comment blocks. Added removal of firewalld at end of process.

--- security_firewalld_disable.sh
+++ security_firewalld_disable.sh
@@ -1,10 +1,58 @@
 #!/usr/bin/env bash
-#
-# Disable firewalld
-#
+##############################################################################
+## Disable firewalld
+##############################################################################
+## Files modified
+##
+##############################################################################
+## License
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License along
+## with this program; if not, write to the Free Software Foundation, Inc.,
+## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+##############################################################################
+## References
+##
+##############################################################################
+## Notes
+##
+##############################################################################
 
-# Stop firewalld
+#timestamp
+echo "** security_firewalld_disable.sh START" $(date +%F-%H%M-%S)
+
+#################
+## SET BASH ERREXIT OPTION
+#################
+set -o errexit
+
+####################
+## STOP SERVICE
+####################
+
 systemctl stop firewalld
 
-# Disable the firewalld service to start automatically
+####################
+## TURN OFF SERVICE
+####################
+
 systemctl disable firewalld
+
+####################
+## REMOVE FIREWALLD
+####################
+
+yum install -y -firewalld
+
+#timestamp
+echo "** security_firewalld_disable.sh COMPLETE" $(date +%F-%H%M-%S)
